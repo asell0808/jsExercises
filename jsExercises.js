edc7434174859df47845292b0332f2b95445fd2a
Anthony Sellers <asell0808@yahoo.com>
2021-09-07 23:49:05 -0700

    Create jsExercises.js

---
+++ jsExercises.js
@@ -0,0 +1,137 @@
+/* JAVASCRIPT TYPES
+-----------------
+1. Number 
+2. String
+3. Boolean
+4. Undefined
+5. Null
+<!-- 6. Symbol (new in ECMAScript 6) -->
+7. Object
+
+JAVASCRIPT COMPARISONS
+-----------------
+
+!==
+===
+>=
+<=
+>
+<
+
+JAVASCRIPT VARIABLES
+-----------------
+var
+<!-- let (new in ECMAScript 6)-->  
+<!-- const (new in ECMAScript 6)-->
+
+JAVASCRIPT CONDITIONALS
+-----------------
+if
+else
+else if
+<!-- ternary operator -->
+<!-- switch -->
+
+
+JAVASCRIPT LOGICAL OPERATORS
+-----------------
+&&
+||
+!
+
+JAVASCRIPT FUNCTIONS
+-----------------
+var a = function name() {}
+function name() {}
+return
+<!-- () => (new in ECMAScript 6) -->
+
+JAVASCRIPT DATA STRUCTURES
+-----------------
+Array
+Object
+
+JAVASCRIPT LOOPING
+-----------------
+for
+while
+do 
+forEach (new in ECMAScript 5) 
+
+
+JAVASCRIPT KEYWORDS
+-----------------
+break
+case
+catch
+class
+const
+continue
+debugger
+default
+delete
+do
+else
+export
+extends
+finally
+for
+function
+if
+import
+in
+instanceof
+new
+return
+super
+switch
+this
+throw
+try
+typeof
+var
+void
+while
+with
+yield
+
+--------------------------------------------------------------------------------------------
+Exercises
+--------------------------------------------------------------------------------------------
+// Guess what answers you would get if you ran this in the
+// Javascript Console in Google Chrome. Once you have an answer  to the questions then 
+// check them by copying them and running it in the console yourself line by line 
+
+
+//Evaluate the below:
+5 + "34"        39
+5 - "4"         1
+10 % 5          0
+5 % 10           5
+"Java" + "Script"  JavaScript
+" " + " "        "  "
+" " + 0          " 0"
+true + true     1
+true + false    1
+false + true    1
+false - true   -1
+3 - 4          -1
+"Bob" - "bill" NaN
+
+
+//Evaluate the below comparisons:
+5 >= 1 True
+0 === 1 True
+4 <= 1 False
+1 != 1 False
+"A" > "B" False
+"B" < "C" True
+"a" > "A" True
+"b" < "A" False
+true === false  false 
+true != true False
+
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+"Hi There! It's " + "\"sunny\" " + "out"
+*/
