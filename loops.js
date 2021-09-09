0559f44d96cfac84cc50fda8e0918ab7a018edc5
Anthony Sellers <asell0808@yahoo.com>
2021-09-08 21:12:52 -0700

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,20 @@
+/*
+----------------------------
+Loops
+----------------------------
+
+var todos = [
+    "Clean room",
+    "Brush teeth",
+    "exercise",
+    "study JS",
+    "eat healthy"
+];
+
+for (var i=0; i < todos.length; i++){
+    todos[i] = todos[i] + "!";
+}
+
+todos;
+
+*/
