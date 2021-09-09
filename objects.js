3470ff9b9acf8faa9d60914e769983c1a1201cdb
Anthony Sellers <asell0808@yahoo.com>
2021-09-08 19:32:02 -0700

    Update objects.js

--- objects.js
+++ objects.js
@@ -3,4 +3,40 @@
 Objects
 ---------------------------
 
+// object with an array and function inside it
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+    spells:["abrakadra", "shazam", "boo"],
+    shout: function(){
+        console.log("RAAAAAAAAAAAAH!");
+    }
+};
+
+// modifying the object
+user.name;
+
+user.favoriteFood = "Spinach";
+
+user.isMarried = true;
+
+// object array
+var list = [
+    {
+        username: "andy",
+        password: "password"
+    },
+    {
+        username: "grigsby",
+        password: "123456"
+    }
+];
+
+// objects and arrays can be empty
+var user2 = {};
+
+array[];
+
 */
