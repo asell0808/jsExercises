39ba365127e2d5e67fc55d507f734900e062ddb7
Anthony Sellers <asell0808@yahoo.com>
2021-09-08 19:02:07 -0700

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,1 +1,24 @@
+/*
+---------------------------
+Arrays
+---------------------------
+var list = ["tiger", "cat", "bear", "bird"];
+console.log(list[1]);
 
+// arrays index from 0,1,2,3... and so on
+// arrays have built in methods
+
+list.shift();
+
+list.pop();
+
+list.push("elephant");
+
+list.concat(["bee", "deer"]);
+
+list.sort();
+
+var myList = ["cat", "bear", "elephant", "bee", "deer"];
+var myNewList = myList.concat(["monkey"]);
+
+*/
