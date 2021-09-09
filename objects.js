f73e26de28df7ac610d271f9c14161d3e6ce0a71
Anthony Sellers <asell0808@yahoo.com>
2021-09-08 19:47:00 -0700

    Update objects.js

--- objects.js
+++ objects.js
@@ -37,6 +37,44 @@ var list = [
 // objects and arrays can be empty
 var user2 = {};
 
-array[];
+var array = array[];
+
+--------------------------
+Simple Facebook
+--------------------------
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+{
+    username: "Ralphie",
+    password: "thisIsAPassword",
+};
+
+var database = [
+    {
+        username: "Ralphie",
+        password: "thisIsAPassword",
+    }
+];
+
+var newsfeed = [
+    {
+        username: "Rick",
+        timeline: "Here is your timeline",
+    },
+    {
+        username: "Mina",
+        timeline: "Here is your timeline",
+    },
+    {
+        username: "Alfred",
+        timeline: "Here is your timeline",
+    }
+];
 
 */
