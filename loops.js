09588832a7c72d3cc52a6fe217d3d8ef06bc41c9
Anthony Sellers <asell0808@yahoo.com>
2021-09-08 21:22:05 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
 /*
 ----------------------------
-Loops
+For Loops
 ----------------------------
+// most common loop
 
 var todos = [
     "Clean room",
@@ -11,9 +12,53 @@ var todos = [
     "eat healthy"
 ];
 
+var todosLengthj = todos.length;
+
 for (var i=0; i < todos.length; i++){
     todos[i] = todos[i] + "!";
 }
+
+todos;
+
+---------------------------
+While Loops
+---------------------------
+var counterOne = 10;
+
+while (counterOne > 0){
+    console.log(counterOne);
+    counterOne--;
+}
+
+---------------------------
+Do Loops
+---------------------------
+// The do loop will run its condition and check it before the while loop runs 
+
+var counterTwo = 10;
+do{
+    console.log(counterTwo);
+    counterTwo--;
+}
+while (counterTwo > 0);
+
+---------------------------
+forEach Loop
+---------------------------
+var todos = [
+    "Clean room",
+    "Brush teeth",
+    "exercise",
+    "study JS",
+    "eat healthy"
+];
+
+function logTodos(todo, i){
+    console.log(todo, i);
+}
+
+todos.forEach(logTodos);
+todosImportant.forEach(logTodos);
 
 todos;
 
